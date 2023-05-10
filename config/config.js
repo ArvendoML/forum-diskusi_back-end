821b7f18057a79f73c50ee1b9ad8a03daf1de1e4
Arvendo Manuel Lextian <arvendomanuel@gmail.com>
2023-05-10 13:32:25 +0700

    database update

--- config/config.js
+++ config/config.js
@@ -20,8 +20,8 @@ module.exports = {
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_production`,
-    // database: process.env.DB_DATABASE,
+    // database: `${process.env.DB_NAME}_production`,
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
